a08c4268472dc4aed142c6fd984d7cb75f8200cf
Daniel Thomas <daniel.m2101281@st.futminna.edu.ng>
2026-04-18 14:54:17 -0700

    updated pages

--- js/passwordreset.js
+++ js/passwordreset.js
@@ -3,25 +3,20 @@ import { supabase } from "./supabaseClient.js";
 const form = document.getElementById("resetForm");
 const message = document.getElementById("message");
 
-console.log("Reset script loaded ✔️");
-
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const email = document.getElementById("email").value;
 
-  console.log("Sending reset for:", email);
-
   const { error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: "https://futomsa-election2026.vercel.app/passwordreset.html"
+    redirectTo: "https://futomsa-election2026.vercel.app/update-password.html"
   });
 
   if (error) {
-    console.log("Supabase error:", error);
     message.textContent = error.message;
     message.style.color = "red";
   } else {
-    message.textContent = "Reset link sent! Check your email.";
+    message.textContent = "Check your email for reset link!";
     message.style.color = "green";
   }
 });
